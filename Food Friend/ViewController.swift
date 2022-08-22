792d956306db0673e524cc51233908a90142f41c
cheesetan_ <tristanchay123@gmail.com>
2022-08-22 11:26:26 +0800

    Initial Commit

---
+++ Food Friend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food Friend
+//
+//  Created by Tristan on 22/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
